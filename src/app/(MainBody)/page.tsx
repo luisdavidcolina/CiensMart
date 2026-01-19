87d5a1b392dc684576ac8ac6a674336ac6c32f9b
Luisdavid Colina <luisdavid.colina@gmail.com>
2026-01-19 19:55:19 -0400

    cambiar layout

--- src/app/(MainBody)/page.tsx
+++ src/app/(MainBody)/page.tsx
@@ -1,57 +1,71 @@
 "use client";
 import NewsLatter from "@/views/Containers/news-letter";
-import Layouts from "@/views/layouts/layout1";
-import CollectionBanner from "@/views/layouts/layout1/collection-banner";
-import CollectionBannerThree from "@/views/layouts/layout1/collection-banner-three";
-import CollectionBannerTwo from "@/views/layouts/layout1/collection-banner-two";
 import HotDeal from "@/views/layouts/layout1/hot-deal";
-import SliderBanner from "@/views/layouts/layout1/slider";
-import TabProduct from "@/views/layouts/widgets/Tab-Product/TabProduct";
-import ContactBanner from "@/views/layouts/widgets/contact-us";
+import Layout5Section from "@/views/layouts/layout5";
+import Blogs from "@/views/layouts/layout5/blog";
+import CollectionBanner from "@/views/layouts/layout5/collectionBanner";
+import SliderBanner from "@/views/layouts/layout5/slider";
+import TabProduct from "@/views/layouts/widgets/Tab-Product/Tab-Product2";
+import Brand from "@/views/layouts/widgets/TopBrand";
+import Contact from "@/views/layouts/widgets/contact-us";
 import DealBanner from "@/views/layouts/widgets/dealBanner";
-import DiscountBanner from "@/views/layouts/widgets/discount-banner";
-import DiscountCoupon from "@/views/layouts/widgets/discountCoupon";
-import InstagramSection from "@/views/layouts/widgets/instagram/instagram1";
+import InstagramSection from "@/views/layouts/widgets/instagram/Instagram2";
 import RatioSquare from "@/views/layouts/widgets/ratio-square";
 import Category from "@/views/layouts/widgets/roundedCategory";
+import Services from "@/views/layouts/widgets/services";
+import SpecialProduct from "@/views/layouts/widgets/slider-product/slider-product2";
 import Testimonial from "@/views/layouts/widgets/testimonial";
-import SpecialProduct from "@/views/layouts/widgets/title-section";
+import { useEffect } from "react";
 
-const Home = () => {
+const Layout5 = () => {
+  useEffect(() => {
+    document.documentElement.classList.remove(localStorage.getItem("color")|| "''");
+    localStorage.setItem("color", "color-5");
+    document.documentElement.classList.add(localStorage.getItem("color")|| "''");
+  }, []);
   return (
     <>
       <NewsLatter />
-      <Layouts>
-        <div className="bg-light">
-          <SliderBanner />
-          <CollectionBanner />
-          <DiscountBanner />
-          <TabProduct effect="icon-inline" />
-          <CollectionBannerTwo />
-          <section className="deal-banner">
-            <DealBanner />
-          </section>
-          <section className="rounded-category">
-            <Category />
-          </section>
-          <section className="box-category section-py-space">
-            <DiscountCoupon />
-          </section>
-          <RatioSquare />
-          <CollectionBannerThree />
-          <HotDeal />
-          <section className="testimonial testimonial-inverse">
-            <Testimonial />
-          </section>
-          <SpecialProduct hoverEffect="icon-inline" />
-          <section className="instagram">
-            <InstagramSection />
-          </section>
-          <ContactBanner />
+      <Layout5Section>
+        <SliderBanner />
+        <div className="ratio_asos">
+          <SpecialProduct type="electronics" />
         </div>
-      </Layouts>
+        <CollectionBanner />
+        <section className="brand-panel">
+          <div className="brand-panel-box bg-silver">
+            <Brand />
+          </div>
+        </section>
+        <section className="services services-inverse section-big-mb-space">
+          <Services />
+        </section>
+        <div className="pt-3">
+          <RatioSquare />
+        </div>
+        <section className="deal-banner deal-banner-inverse section-big-mt-space">
+          <DealBanner />
+        </section>
+        <section className="rounded-category rounded-category-inverse">
+          <Category />
+        </section>
+        <div className="section-pb-space">
+          <TabProduct type={["electronics"]} />
+        </div>
+        <section className="testimonial testimonial-inverse section-mb-space">
+          <Testimonial />
+        </section>
+        <HotDeal />
+        <div className="tab-slider-sec">
+          <Blogs />
+        </div>
+        <section className="instagram section-big-mt-space  section-big-py-space b-g-white">
+          <InstagramSection />
+        </section>
+        <Contact />
+      </Layout5Section>
     </>
   );
 };
 
-export default Home;
+export default Layout5;
