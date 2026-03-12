ac68013ecfa25bf52ef75462d2b0107afdc393a9
Luisdavid Colina <luisdavid.colina@gmail.com>
2026-03-11 22:58:51 -0400

    upd

--- src/views/Containers/Header/widgets/mobile-search.tsx
+++ src/views/Containers/Header/widgets/mobile-search.tsx
@@ -6,16 +6,21 @@ import { useRouter } from "next/navigation";
 const MobileSearch: NextPage = () => {
   const router = useRouter();
   const [searchTerm, setSearchTerm] = React.useState("");
+  const [isOpen, setIsOpen] = React.useState(false);
 
   useEffect(() => {
-    document.getElementById("search-overlay")!.style.display = "none";
-  });
+    document.body.style.overflow = isOpen ? "hidden" : "visible";
+    return () => {
+      document.body.style.overflow = "visible";
+    };
+  }, [isOpen]);
+
   const closeSearch = () => {
-    document.getElementById("search-overlay")!.style.display = "none";
+    setIsOpen(false);
   };
 
   const openSearch = () => {
-    document.getElementById("search-overlay")!.style.display = "flex";
+    setIsOpen(true);
   };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -31,36 +36,39 @@ const MobileSearch: NextPage = () => {
       <li className="onhover-div mobile-search">
         <i className="icon-search" onClick={openSearch}></i>
       </li>
-      <div id="search-overlay" className="search-overlay">
-        <div>
-          <span className="close-mobile-search" onClick={closeSearch} title="Close Overlay">
-            ×
-          </span>
-          <div className="overlay-content">
-            <Container>
-              <Row>
-                <Col xl="12">
-                  <Form onSubmit={handleSubmit}>
-                    <FormGroup>
-                      <Input
-                        type="text"
-                        className="form-control"
-                        id="mobileSearchInput"
-                        placeholder="Search a Product"
-                        value={searchTerm}
-                        onChange={(event) => setSearchTerm(event.target.value)}
-                      />
-                    </FormGroup>
-                    <Button type="submit" className="btn btn-primary">
-                      <i className="fa fa-search"></i>
-                    </Button>
-                  </Form>
-                </Col>
-              </Row>
-            </Container>
+      {isOpen && (
+        <div id="search-overlay" className="search-overlay">
+          <div>
+            <span className="close-mobile-search" onClick={closeSearch} title="Close Overlay">
+              ×
+            </span>
+            <div className="overlay-content">
+              <Container>
+                <Row>
+                  <Col xl="12">
+                    <Form onSubmit={handleSubmit}>
+                      <FormGroup>
+                        <Input
+                          type="text"
+                          className="form-control"
+                          id="mobileSearchInput"
+                          placeholder="Search a Product"
+                          value={searchTerm}
+                          onChange={(event) => setSearchTerm(event.target.value)}
+                          autoFocus
+                        />
+                      </FormGroup>
+                      <Button type="submit" className="btn btn-primary">
+                        <i className="fa fa-search"></i>
+                      </Button>
+                    </Form>
+                  </Col>
+                </Row>
+              </Container>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
