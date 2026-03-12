005db9bd799efb47fdc2d0ed7f2d548cef464eae
Luisdavid Colina <luisdavid.colina@gmail.com>
2026-03-11 23:02:11 -0400

    upd

--- src/app/(MainBody)/layout.tsx
+++ src/app/(MainBody)/layout.tsx
@@ -1,5 +1,6 @@
 "use client";
 import dynamic from "next/dynamic";
+import { useEffect } from "react";
 import { CartProvider } from "@/helpers/cart/cart.provider";
 import { CompareProvider } from "@/helpers/compare/compare.provider";
 import { CurrencyContextProvider } from "@/helpers/currency/CurrencyContext";
@@ -11,6 +12,18 @@ import { AuthProvider } from "@/helpers/auth/auth.context";
 const TaptoTop = dynamic(() => import("@/views/Containers/TapTop"), { ssr: false });
 const ToastContainer = dynamic(() => import("react-toastify").then((mod) => mod.ToastContainer), { ssr: false });
 const layout = ({ children }: { children: React.ReactNode }) => {
+  useEffect(() => {
+    const themeClasses = ["color-1", "color-2", "color-3", "color-4", "color-5", "color-6"];
+    const savedColor = localStorage.getItem("color") || "color-5";
+
+    themeClasses.forEach((themeClass) => {
+      document.documentElement.classList.remove(themeClass);
+    });
+
+    document.documentElement.classList.add(savedColor);
+    localStorage.setItem("color", savedColor);
+  }, []);
+
   return (
     <AuthProvider>
       <div>
